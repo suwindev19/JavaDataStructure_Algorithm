e9615b10b0e9bfb6d3a914e586800ff94b4697fe
Su_Win <suwindev19@gmail.com>
2020-11-17 10:58:49 +1100

    Notes added to Node.java

--- src/DataStructure_CourseNotes/week1/Node.java
+++ src/DataStructure_CourseNotes/week1/Node.java
@@ -1,5 +1,11 @@
 package DataStructure_CourseNotes.week1;
+/*
+A Single node
+- contains data -> integer, double or custom object
+- contains a reference pointing to the next node in the linked list
 
+Each node is composed of a data
+ */
 public class Node {
     int data;
     Node next;
--- src/DataStructure_CourseNotes/week1/Test.java
+++
@@ -1,4 +0,0 @@
-package DataStructure_CourseNotes.week1;
-
-public class gTest {
-}

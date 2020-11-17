8efd394a834fff1a080a905d73c2cd10b0fc5f82
Su_Win <suwindev19@gmail.com>
2020-11-17 11:45:25 +1100

    Restructured DSA repo

--- src/DataStructure_CourseNotes/week1/LinkedList.java
+++ src/DataStructure_CourseNotes/week1/LinkedList.java
@@ -1,5 +1,32 @@
 package DataStructure_CourseNotes.week1;
+/*
+Linked List Operations
+=====================
+- inserting items at the beginning of the linked list
+    - very simple, you just have to update the references -> O(1) Time complexity
+    - linkedList.insertAtStart(10); 10 Null, insert 4 , point to 10 , set the pointer point to the next node
+    - Array is great for random access
+- inserting items at the end of the Linked List
+    - You have to traverse the whole linked list to find the last node
+    - How do you find the last node?
+    - You know that the last node is pointing to NULL
+    - You have to update the references when you get there O(n) Time
+    - linkedList.insertAtEnd(25)
+    - 12, 4, 123, -7, 10 , null
+    - root of the node is 12. Have to go to the last node. Keep traversing until the pointer is pointing to null
+    - 10 needs to point to item to be inserted (25) therefore O(1) + O(n) = O(n)
 
+- Removing at the beginning
+    - removing item at the beginning of the list is always very fast
+    - you do not have to search the item
+    - you have to update the references accordingly
+    - O(1)
+    - linkedList.removeStart()
+- Removing at the given point
+    - Search for the given item (which may take a lot of time
+    - if the item is at the end of the list, O(n)
+    - linkedList.reove(10)
+ */
 public class LinkedList {
     Node head;
 
--- src/DataStructure_CourseNotes/week1/Node.java
+++ src/DataStructure_CourseNotes/week1/Node.java
@@ -4,7 +4,30 @@ A Single node
 - contains data -> integer, double or custom object
 - contains a reference pointing to the next node in the linked list
 
-Each node is composed of a data
+Each node is composed of a data and a reference/ link to the next node in the sequence.
+Simple and very common data structure!
+They can be used to implement several other common data types: stacks, queues
+Simple linked lists by themselves do not allow random acccess to the data , therefore you cannot use indexes to get them.
+Many basic operations such as obtaining the last node of the list or finding a node that contains a given data or locating the place where a new node should be inserted - requires sequential scanning of most or all of the list elements
+
+
+Advantages
+- linked lists are dynamic data structures (array are not!)
+- It can allocate the needed memory in run time
+- Very efficient if we want to manipulate the first elements
+
+Easy implementation
+- can store  items with different sizes : array assumes every element to be exactly the same
+- It is easier for a linked list to grow organically. An array' size needs to be known ahead of time or re-created when it needs to grow
+
+
+Disadvantages
+- Waste memory because of references
+- Node in a linked list must be read in order from the beginning as linked lists have sequential access (array items can be reached via indexes in O(1) time !
+- Difficulties arise in Linked Lists when it comes to reverse traversing.
+- Singly linked list are extremely difficult to nagivate backwards
+- Solution: Doubly linked lists -> easier to read, but memory is wasted in allocating space for a back pointer
+
  */
 public class Node {
     int data;

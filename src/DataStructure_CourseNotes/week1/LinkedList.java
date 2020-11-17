e9206e274dc87296148fa863a7ba016139a9ba59
Su_Win <suwindev19@gmail.com>
2020-11-17 12:18:04 +1100

    Added LinkedList vs Array - search, delete, insert, memory

--- src/DataStructure_CourseNotes/week1/LinkedList.java
+++ src/DataStructure_CourseNotes/week1/LinkedList.java
@@ -26,6 +26,43 @@ Linked List Operations
     - Search for the given item (which may take a lot of time
     - if the item is at the end of the list, O(n)
     - linkedList.reove(10)
+
+- Doubly Linked List
+    - We can get from 4 to 25 because we just have to hop to the next node.
+    - Node class has two references, one pointing to the next node, one pointing to the previous node
+    - It is not memory friendly, you have to store lots of references
+    - But there is not need to track the previous node during traversal
+
+
+    Linked List vs Array
+Searching
+    - Search operation yields the same result for both data structure
+    - ArrayList search operation is pretty fast compared to LinkedList search operation
+    - You can use random access arrays : getItem( int index) which is O(1) Time
+    - LinkedList performance is O(n) Time
+    - WHy?
+    - ArrayList maintains index based system for its elment as it uses array data structure implicitly which makes it faster for searching an element in the list
+    - On the other hand LinkedList requires the traversal through all the items for searching an element
+
+Deletion
+    - LinkedList remove operation takes O(1) time if we remove items from the beginning and usually this is the case
+    - ArrayList : removing first element ( so at the beginning) takes O(n) time, removing the last item takes O(1) times
+    - But on average, we have to reconstruct the array when removing
+    - Therefore in conclusion, linkedlist is better for this option
+    - Why?
+    - Linked List basically operates with pointers : removal only requires change in the pointer location which can be done very fast
+Memory Management
+
+    - Arrays do not need any extra memory
+    - LinkedList do need extra memory because of references / pointers
+    - In this aspect, arrays are better, they are memory friendly.
+
+    Operation               LinkedList              Arrays
+    ---------               ----------              ------
+    search                  O(n)                     O(1)
+    Insert at the start     O(1)                     O(n)
+    Insert at the end       O(n)                     O(1)
+    Waste space             O(n)                     0
  */
 public class LinkedList {
     Node head;
